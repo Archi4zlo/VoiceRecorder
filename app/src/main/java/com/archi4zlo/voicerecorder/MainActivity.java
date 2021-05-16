4c2ada9a2f77dbf337bcc0d49163ced34d1ddb67
Anton Sych <anton.sych.job@gmail.com>
2021-05-16 22:46:30 +0300

    add toolbar with tabLayout, viewPager and ViewPagerAdapter

--- app/src/main/java/com/archi4zlo/voicerecorder/MainActivity.java
+++ app/src/main/java/com/archi4zlo/voicerecorder/MainActivity.java
@@ -1,14 +1,41 @@
 package com.archi4zlo.voicerecorder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+
+import com.archi4zlo.voicerecorder.Fragments.RecorderFragment;
+import com.archi4zlo.voicerecorder.Fragments.RecordingsListFragment;
+import com.archi4zlo.voicerecorder.adapters.ViewPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        setSupportActionBar(toolbar);
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new RecorderFragment(), "Recorder");
+        viewPagerAdapter.addFragment(new RecordingsListFragment(),"Recordings");
+        viewPager.setAdapter(viewPagerAdapter);
     }
 }
